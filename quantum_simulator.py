a6d7702d662dad456c18f46c66a123f283a507e9
Walikuperek <56729104+Walikuperek@users.noreply.github.com>
2024-01-07 12:21:19 +0100

    Init commit

---
+++ quantum_simulator.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+
+isq2 = 1.0 / (2.0 ** 0.5)
+
+
+class QuantumState:
+    def __init__(self, n):
+        self.n = n
+        self.state = np.zeros(2 ** self.n, dtype=np.complex)
+        self.state[0] = 1
+
+    # apply transformation t to bit i
+    # (or i and i+1 in case of binary gates)
+    def op(self, t, i):
+        # I_{2^i}
+        eyeL = np.eye(2 ** i, dtype=np.complex)
+
+        # I_{2^{n-i-1}}
+        # t.shape[0]**0.5 denotes how many bits t applies to
+        # in case of NOT, t.shape[0]**0.5 == 1
+        eyeR = np.eye(2 ** (self.n - i - int(t.shape[0] ** 0.5)),
+                      dtype=np.complex)
+
+        # eyeL ⊗ t ⊗ eyeR
+        t_all = np.kron(np.kron(eyeL, t), eyeR)
+
+        # apply transformation to state (multiplication)
+        self.state = np.matmul(t_all, self.state)
+
+    # Hadamard gate
+    def hadamard(self, i):
+        h_matrix = isq2 * np.array([
+            [1, 1],
+            [1, -1]
+        ])
+        self.op(h_matrix, i)
+
+    # T gate
+    def t(self, i):
+        t_matrix = np.array([
+            [1, 0],
+            [0, isq2 + isq2 * 1j]
+        ])
+        self.op(t_matrix, i)
+
+    # S gate
+    def s(self, i):
+        s_matrix = np.array([
+            [1, 0],
+            [0, 0 + 1j]
+        ])
+        self.op(s_matrix, i)
+
+    # CNOT gate
+    def cnot(self, i):
+        cnot_matrix = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1],
+            [0, 0, 1, 0]
+        ])
+        self.op(cnot_matrix, i)
+
+    # Swap two qubits
+    def swap(self, i):
+        swap_matrix = np.array([
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1]
+        ])
+        self.op(swap_matrix, i)
+
+
+# Usage
+computer = QuantumState(2)  # 2 qubits [1 0 0 0]
+computer.hadamard(0)
+print(computer.state)  # [0.70710678 0.70710678 0.         0.        ]
+
+computer2 = QuantumState(2)  # 2 qubits [1 0 0 0]
+computer2.cnot(0)
+print(computer2.state)  # [1. 0. 0. 0.]
+
+computer3 = QuantumState(2)  # 2 qubits [1 0 0 0]
+computer3.swap(0)
+print(computer3.state)  # [1. 0. 0. 0.]
+
+computer4 = QuantumState(2)  # 2 qubits [1 0 0 0]
+computer4.t(0)
+print(computer4.state)  # [1.  +0.j 0.  +0.j 0.  +0.j 0.70710678+0.j]
+
+computer5 = QuantumState(2)  # 2 qubits [1 0 0 0]
+computer5.s(0)
+print(computer5.state)  # [1. +0.j 0. +0.j 0. +0.j 0. +1.j]
---
+++ regular-computer-simulator.ts
@@ -0,0 +1,192 @@
+`
+Regular Computer Simulator
+
+This is a simulator of a regular computer. Written in TypeScript due to clearer type syntax than Python.
+
+Constraints:
+- base on 0 and 1 (binary system)
+- only whole numbers are allowed (no floating point numbers)
+- only positive numbers are allowed (no negative numbers, can be implemented easily with first bit as a sign bit)
+
+Computer - *computer* comes from word *compute* (to count something)
+and *er* (a person or thing that does something). So computer is
+a thing that counts something. In this case it counts bits.
+
+Author:  Kacper Walczak
+Created at:  02/01/2024
+`
+
+type Bit = 0 | 1;
+type WordLength = 1 | 2 | 4 | 8 | 16 | 32 | 64 | 128 | 256 | 512 | 1024;
+type Word = Bit[];
+
+const WORD_LENGTH: WordLength = 8; // specifies how many bits are in a word
+
+// 8-bit architecture, word example: [0,    0,  0,  0, 0, 0, 0, 0]
+//                     values:       [128, 64, 32, 16, 8, 4, 2, 1]
+type WordFactoryOpts = { length?: WordLength, bitVal?: Bit };
+function wordFactory(opts?: WordFactoryOpts): Word {
+    const defaults = {length: WORD_LENGTH, bitVal: 0};
+    const wordDefault = Object.assign(defaults, opts);
+    return Array(wordDefault.length).fill(wordDefault.bitVal);
+}
+
+function positiveIntegerToWord(integerToMap: number): Word {
+    // 83 / 2 = 41, remainder 1
+    // 41 / 2 = 20, remainder 1
+    // 20 / 2 = 10, remainder 0
+    // 10 / 2 = 5,  remainder 0
+    // 5 / 2 = 2,   remainder 1
+    // 2 / 2 = 1,   remainder 0
+    // 1 / 2 = 0,   remainder 1
+    // result = 1010011 = 83
+
+    return wordFactory().map((_, index) => {
+        const bitValue = 2 ** (WORD_LENGTH - index - 1); // 2 ** (8 - 0 - 1) = 2 ** 7 = 128
+        const bit: Bit = integerToMap >= bitValue ? 1 : 0;
+        integerToMap -= bit * bitValue;
+        return bit;
+    });
+}
+
+function wordToPositiveInteger(word: Word): number {
+    return word.reduce((accumulator, currentValue, index) => {
+        const bitValue = 2 ** (WORD_LENGTH - index - 1);
+        return accumulator + currentValue * bitValue as Bit;
+    }, 0);
+}
+
+const computer = {
+    add(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 + bit2 >= 1 ? 1 : 0;
+        });
+    },
+    subtract(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 - bit2 >= 0 ? 1 : 0;
+        });
+    },
+    multiply(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 * bit2 >= 1 ? 1 : 0;
+        });
+    },
+    divide(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 / bit2 >= 1 ? 1 : 0;
+        });
+    },
+    shiftBitsLeft(word: Word): Word {
+        // 1 0 1 0 0 1 1 0
+        // 0 1 0 0 1 1 0 0
+        return wordFactory().map((_, index) => {
+            const bit = word[index];
+            const nextBit = word[index + 1];
+            return nextBit ? nextBit : bit;
+        });
+    },
+    shiftBitsRight(word: Word): Word {
+        // 1 0 1 0 0 1 1 0
+        // 0 1 0 1 0 0 1 1
+        return wordFactory().map((_, index) => {
+            const bit = word[index];
+            const previousBit = word[index - 1];
+            return previousBit ? previousBit : bit;
+        });
+    },
+    compare(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 === bit2 ? 1 : 0;
+        });
+    },
+    and(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 && bit2 ? 1 : 0;
+        });
+    },
+    or(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 || bit2 ? 1 : 0;
+        });
+    },
+    xor(word1: Word, word2: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit1 = word1[index];
+            const bit2 = word2[index];
+            return bit1 !== bit2 ? 1 : 0;
+        });
+    },
+    not(word: Word): Word {
+        return wordFactory().map((_, index) => {
+            const bit = word[index];
+            return bit ? 0 : 1;
+        });
+    },
+    increment(word: Word): Word {
+        return this.add(word, positiveIntegerToWord(1));
+    },
+    decrement(word: Word): Word {
+        return this.subtract(word, positiveIntegerToWord(1));
+    },
+    isZero(word: Word): Word {
+        return this.compare(word, wordFactory());
+    },
+    isNotZero(word: Word): Word {
+        return this.not(this.isZero(word));
+    },
+    isLessThan(word1: Word, word2: Word): Word {
+        return this.compare(word1, word2);
+    },
+    isGreaterThan(word1: Word, word2: Word): Word {
+        return this.compare(word2, word1);
+    },
+    isLessThanOrEqual(word1: Word, word2: Word): Word {
+        return this.or(this.isLessThan(word1, word2), this.isZero(word1));
+    },
+    isGreaterThanOrEqual(word1: Word, word2: Word): Word {
+        return this.or(this.isGreaterThan(word1, word2), this.isZero(word1));
+    }
+}
+
+console.warn(`
+    ✅ values:       [128, 64, 32, 16, 8, 4, 2, 1]
+    123          = ${positiveIntegerToWord(123)}
+    0            = ${positiveIntegerToWord(0)}
+    1            = ${positiveIntegerToWord(1)}
+    2            = ${positiveIntegerToWord(2)}
+    4            = ${positiveIntegerToWord(4)}
+    8            = ${positiveIntegerToWord(8)}
+    16           = ${positiveIntegerToWord(16)}
+    32           = ${positiveIntegerToWord(32)}
+    64           = ${positiveIntegerToWord(64)}
+    128          = ${positiveIntegerToWord(128)}
+    256          = ${positiveIntegerToWord(256)}
+
+    12 + 34      = ${positiveIntegerToWord(12 + 34)}
+    123 + 1      = ${positiveIntegerToWord(123 + 1)}
+
+    ✅ shifting bits:
+    1 >> 0     = ${positiveIntegerToWord(1 >> 0)} ${1 >> 0}
+    123 >> 0   = ${positiveIntegerToWord(123 >> 0)} ${123 >> 0}
+    123 >> 1   = ${positiveIntegerToWord(123 >> 1)} ${123 >> 1}
+    123 >> 2   = ${positiveIntegerToWord(123 >> 2)} ${123 >> 2}
+    123 >> 3   = ${positiveIntegerToWord(123 >> 3)} ${123 >> 3}
+
+    ✅ computer.add(positiveIntegerToWord(12), positiveIntegerToWord(34)) = ${computer.add(positiveIntegerToWord(12), positiveIntegerToWord(34))}
+    ✅ wordToPositiveInteger(computer.add(positiveIntegerToWord(12), positiveIntegerToWord(34))) = ${wordToPositiveInteger(computer.add(positiveIntegerToWord(12), positiveIntegerToWord(34)))}
+`)
